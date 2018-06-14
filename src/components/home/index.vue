748a4967ac5958e9b0391bba099aeee7636387b1
qitmac000455 <qitmac000455@qitmac000455deMacBook-Pro.local>
2018-06-14 19:06:40 +0800

    check

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -2,7 +2,7 @@
     <div class="page">
         <div class="header">
             <div class="menu">
-                <img src="../../assets/logo.png" class="logo">
+                <img src="../../../sassets/logo.png" class="logo">
                 <ul>
                     <li>终身学习</li>
                     <li>终身学习</li>
