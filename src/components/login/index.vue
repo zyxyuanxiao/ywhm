0733b2687ebe86ec87be78f73cc3479aaba21ee3
mzry <546340932@qq.com>
2018-06-26 19:17:15 +0800

    login0626

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,26 +1,37 @@
 <template>
-<div class="login-dialog">
-	<div class="info-dialog">
-			<h3>欢迎来到 「亿万毫米」</h3>
+	<div>
+		<Button @click="modal1 = true">Set the width</Button>
+		<Modal
+		v-model="modal1"
+		width="420" height="340">
+			<div class="dialog"
+			class-name="vertical-center-modal">
+				<h3>欢迎来到「亿万毫米」</h3>
+				<Form ref="formInline" :model="formInline" :rules="ruleInline">
+					<FormItem prop="user">
+						<Input type="text" v-model="formInline.user" placeholder="Username">
+							<Icon type="ios-person-outline" slot="prepend" size="20"></Icon>
+						</Input>
+					</FormItem>
+					<FormItem prop="password">
+						<Input type="password" v-model="formInline.password" placeholder="Password">
+							<Icon type="ios-locked-outline" slot="prepend" size="20">></Icon>
+						</Input>
+					</FormItem>
+					<FormItem>
+						<Button type="primary" @click="handleSubmit('formInline')">登录</Button>
+					</FormItem>
+				</Form>
+				<p>没有账号？<router-link to="/home/register">现在去注册</router-link></p>
+			</div>
+		</Modal>
 	</div>
-	<form>
-		<div>
-			<Icon type="person" ></Icon>
-			<input type="text" v-model="formInline.user" placeholder="Username" />
-		</div>
-		<div>
-			<Icon type="locked"></Icon>
-			<input type="password" v-model="formInline.password" placeholder="Password" />
-		</div>
-		<Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
-		<p>没有账号？<router-link to="/home/register">现在去注册</router-link></p>
-	</form>
-</div>
 </template>
 <script>
 	export default {
 		data () {
 			return {
+				modal1: false,
 				formInline: {
 					user: '',
 					password: ''
@@ -49,7 +60,53 @@
 		}
 	}
 </script>
-
 <style scoped>
-
+.dialog {
+	margin: 30px auto 21px;
+	width: 290px;
+}
+h3 {
+	margin-bottom: 20px;
+	font-size: 28px;
+	font-weight: 400;
+}
+.ivu-btn{
+	width: 100%;
+	height: 36px;
+}
+a {
+	color: #23acf1;
+}
+.ivu-form-item-content {
+	border-color: #ccc;
+	height: 36px;
+}
+.ivu-input-group-append, .ivu-input-group-prepend {
+	border: 1px solid #ccc;
+	background-color: #fff;
+	width: 36px;
+}
+.ivu-input {
+	padding: 18px 7px;
+	border: 1px solid #ccc;
+}
+.ivu-modal-footer {
+	display: none;
+}
+/* .ivu-btn-primary {
+	background-color: #23acf1;
+    border-color: #22acf1;
+} */
+.ivu-form-item-content {
+	height: 36px;
+	line-height: 36px;
+}
+.ivu-input-group .ivu-input {
+	height: 36px !important;
+	line-height: 36px!important;
+}
+.ivu-input-group-append, .ivu-input-group-prepend{
+	border: 1px solid #ccc;
+}
 </style>
+
